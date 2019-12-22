71c788ab905c5fac54136dd6dff5bd71fc961182
muerroui <errouissimusta@gmail.com>
2019-12-22 10:08:31 +0100

    change database name

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { scraperModule } from './scraper/scraper.module';
     port: 3306,
     username: 'user',
     password: 'password',
-    database: 'api',
+    database: 'phonecomparator',
     //synchronize: true,
     entities: [__dirname + '/**/**/entity/**.entity{.ts,.js}'],
   }), PhoneModule, scraperModule],
